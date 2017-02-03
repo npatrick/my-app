60e8b1c4b652e87ab552064dce336e88df19ebfc
Nicko Romana <wazzupnickz@gmail.com>
2017-02-02 18:35:24 -0800

    fix footer links to open new tabs and the nav color

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,14 +4,14 @@ class Footer extends Component {
 	render() {
 		return (
 			<footer className="w3-content w3-padding-medium w3-text-grey w3-xlarge">
-		    <a href="https://www.linkedin.com/in/neilromana/">
+		    <a href="https://www.linkedin.com/in/neilromana/" target="_blank">
 		    	<i className="fa fa-linkedin w3-hover-text-indigo"></i>
+		    </a>
+		    <a href="https://github.com/npatrick" target="_blank">
+		    	<i className="fa fa-github w3-hover-text-purple"></i>
 		    </a>
 		    <a href="">
 		    	<i className="fa fa-facebook-official w3-hover-text-indigo"></i>
-		    </a>
-		    <a href="">
-		    	<i className="fa fa-instagram w3-hover-text-purple"></i>
 		    </a>
 		    <a href="">
 		    	<i className="fa fa-twitter w3-hover-text-light-blue"></i>
