66f1084f52d73f556c6f8f8d6df0b8e7498bb4ad
Nicko Romana <wazzupnickz@gmail.com>
2017-02-09 06:13:15 -0800

    add navigation component

--- src/components/home.js
+++ src/components/home.js
@@ -7,6 +7,7 @@ class Home extends Component {
 			<header className="w3-container w3-padding-32 w3-center" id="home">
 		    <h1 className="w3-jumbo"><span className="w3-hide-small">I'm</span> Neil Romana</h1>
 		    <p>Software Engineer.</p>
+		    <br />
 		    <img src={selfpic} alt="myself" className="w3-image" style={{maxWidth:"100%", height:"auto"}} />
 		  </header>
 		);
---
+++ src/components/navigation.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export default class Navigation extends Component {
+  render() {
+    return (
+      <div className="navigation">
+        <nav className="w3-sidenav w3-center w3-small w3-hide-small">
+          <a className="w3-padding-large w3-black" href="#">
+            <i className="fa fa-home w3-xxlarge"></i>
+            <p>HOME</p>
+          </a>
+          <a className="w3-padding-large w3-hover-black" href="#portfolio">
+            <i className="fa fa-user w3-xxlarge"></i>
+            <p>PORTFOLIO</p>
+          </a>
+          <a className="w3-padding-large w3-hover-black" href="#blog">
+            <i className="fa fa-eye w3-xxlarge"></i>
+            <p>BLOG</p>
+          </a>
+          <a className="w3-padding-large w3-hover-black" href="#contact">
+            <i className="fa fa-envelope w3-xxlarge"></i>
+            <p>CONTACT</p>
+          </a>
+        </nav>
+
+        <div className="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
+          <ul className="w3-navbar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
+            <li className="w3-left" style={{width:"25% !important"}}><a href="#">HOME</a></li>
+            <li className="w3-left" style={{width:"25% !important"}}><a href="#portfolio">PORTFOLIO</a></li>
+            <li className="w3-left" style={{width:"25% !important"}}><a href="#blog">BLOG</a></li>
+            <li className="w3-left" style={{width:"25% !important"}}><a href="#contact">CONTACT</a></li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
