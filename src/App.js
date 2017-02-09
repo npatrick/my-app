dea6545c416a1219f75c07ef9ad789fba060c46a
Nicko Romana <wazzupnickz@gmail.com>
2017-02-08 21:13:38 -0800

    add style dir and move css files into it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../style/App.css';
 import Home from './components/home.js';
 import Portfolio from './components/portfolio.js';
 import Contact from './components/contact.js';
