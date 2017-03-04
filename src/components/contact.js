fa6581e879b085a01477b8dae61076fc44e3a615
Nicko Romana <wazzupnickz@gmail.com>
2017-03-04 14:49:34 -0800

    add validation function on contact fields

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,16 +10,35 @@ export default class Contact extends Component {
     action: ' Send Message' 
   };
 
-  handleClick(event) {
+  validateEmail(email) {
+    const reg =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return reg.test(email);
+  }
+
+  handleSubmit(event) {
     const that = this;
+    const { name, email, subject, message } = this.state;
     event.preventDefault();
+    // checks for field inputs
+    if (name.length < 1) {
+      return alert('You need to fill the Name field');
+    }
+    if (!this.validateEmail(email)) {
+      return alert('Invalid Email');
+    }
+    if(subject.length < 1) {
+      return alert('You Need to fill the Subject field');
+    }
+    if(message.length < 1) {
+      return alert('You need to fill the Message content');
+    }
 
     let data = {
-      name: this.state.name,
-      email: this.state.email,
-      subject: this.state.subject,
-      message: this.state.message
-    }
+      name: name,
+      email: email,
+      subject: subject,
+      message: message
+    };
 
     $.ajax({
       type: 'POST',
@@ -33,7 +52,7 @@ export default class Contact extends Component {
     })
     .fail(err => console.log('error: ', err));
   }
-
+  // render based of action state
   renderButton() {
     return (
       <button className="w3-btn w3-light-grey w3-padding-large">
@@ -54,13 +73,14 @@ export default class Contact extends Component {
         </div><br />
         <p>Lets get in touch. Send me a message:</p>
 
-        <form id="contact-form" onSubmit={this.handleClick.bind(this)}>
-          <p><input className="w3-input w3-padding-16" type="text" required placeholder="Name" 
+        <form id="contact-form" onSubmit={this.handleSubmit.bind(this)}>
+          <p><input className="w3-input w3-padding-16" type="text" required placeholder="Name *" 
             onChange={name => this.setState({ name: name.target.value, action: ' Send Message' })} />
           </p>
-          <p><input className="w3-input w3-padding-16" type="text" required placeholder="Email" onChange={email => this.setState({ email: email.target.value, action: ' Send Message' })} /></p>
-          <p><input className="w3-input w3-padding-16" type="text" required placeholder="Subject" onChange={subject => this.setState({ subject: subject.target.value, action: ' Send Message' })} /></p>
-          <p><textarea className="w3-input w3-padding-16" type="text" required placeholder="Message" onChange={message => this.setState({ message: message.target.value, action: ' Send Message' })} /></p>
+          <p><input className="w3-input w3-padding-16" type="text" required placeholder="Email *" onChange={email => this.setState({ email: email.target.value, action: ' Send Message' })} /></p>
+          <p><input className="w3-input w3-padding-16" type="text" required placeholder="Subject *" onChange={subject => this.setState({ subject: subject.target.value, action: ' Send Message' })} /></p>
+          <p><textarea className="w3-input w3-padding-16" type="text" required placeholder="Message *" onChange={message => this.setState({ message: message.target.value, action: ' Send Message' })} /></p>
+          <p> * required field</p>
           <p>{this.renderButton()}</p>
         </form>
       </div>
