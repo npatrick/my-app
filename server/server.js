67a46ece59d7e3267cc601da86f778428048f233
Nicko Romana <wazzupnickz@gmail.com>
2017-03-06 23:42:58 -0800

    add recaptcha basic structure on backend

--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
+const Recaptcha = require('recaptcha');
+
+const recaptcha = new Recaptcha({
+	secret: 'SECRET_KEY',
+	verbose: true
+});
 
 // Create the Express application:
 const app = express();
@@ -20,6 +26,25 @@ app.use(express.static(path.join(__dirname, '../build')));
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../build', 'index.html')));
+
+app.post('/check', (req, res) => {
+	const userResponse = req.query['g-recaptcha-response'];
+
+	recaptcha.checkResponse(userResponse, (error, response) => {
+		if(error) {
+			// an internal error
+			res.status(400).render('400', {
+				message: error.toString()
+			});
+			return;
+		}
+		if(response.success) {
+			// return json
+		} else {
+			// return json
+		}
+	})
+});
 
 app.post('/sendemail', (req, res) => {
 	console.log('POST COMING: ', req.body);
